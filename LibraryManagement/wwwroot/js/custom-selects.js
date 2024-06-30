307f64626fe898642df1b9406e4c9a0ae74f2ee4
Ozymadias11 <natchkebiadima1@gmail.com>
2024-06-30 18:52:16 +0400

    added create links in dropdown when no result found, optimized view for createbook

---
+++ LibraryManagement/wwwroot/js/custom-selects.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    $('#authors').select2({
+        placeholder: 'Select authors',
+        multiple: true,
+        allowClear: true,
+        language: {
+            noResults: function () {
+                return 'No results found. <a href="/Author/CreateAuthor" class="create-new-author">Create new author</a>';
+            }
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        }
+    });
+
+    $('#publishers').select2({
+        placeholder: 'Select Publishers',
+        multiple: true,
+        allowClear: true,
+        language: {
+            noResults: function () {
+                return 'No results found. <a href="/Publisher/CreatePublisher" class="create-new-publisher">Create new Publisher</a>';
+            }
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        }
+    });
+
+    $('#Categories').select2({
+        placeholder: 'Select Categories',
+        multiple: true,
+        allowClear: true,
+        language: {
+            noResults: function () {
+                return 'No results found. <a href="/Category/CreateCategory" class="create-new-category">Create new Category</a>';
+            }
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        }
+    });
+
+    // Handle click on "Create new" links
+    $(document).on('click', '.create-new-author, .create-new-publisher, .create-new-category', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        window.location.href = $(this).attr('href');
+    });
+});
