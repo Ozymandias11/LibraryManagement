5b39a5ef6e9f41e35a7a56346cd13cb04959a929
Ozymadias11 <natchkebiadima1@gmail.com>
2024-07-23 10:07:49 +0400

    added repository unit tests for authors with bogus library for convenient fake data generation

---
+++ UnitTests/TestData/AuthorFaker.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Library.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.TestData
+{
+    public class AuthorFaker
+    {
+        public static Faker<Author> Create()
+        {
+            return new Faker<Author>()
+                .RuleFor(a => a.AuthorId, f => Guid.NewGuid())
+                .RuleFor(a => a.FirstName, f => f.Name.FirstName())
+                .RuleFor(a => a.LastName, f => f.Name.LastName())
+                .RuleFor(a => a.DateOfBirth, f => f.Date.Past(80))
+                .RuleFor(a => a.CreatedDate, f => f.Date.Recent())
+                .RuleFor(a => a.DeletedDate, f => null);
+        }
+
+    }
+}
