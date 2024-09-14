3b5104d3121cd85479a338c880eb0866cb514e94
Ozymadias11 <natchkebiadima1@gmail.com>
2024-09-14 15:10:52 +0400

    started working on RDLC reports

---
+++ LibraryManagement/Controllers/ReportController.cs
@@ -0,0 +1,45 @@
+﻿using Library.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
+
+namespace LibraryManagement.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ReportController(IServiceManager serviceManager, IWebHostEnvironment webHostEnvironment)
+        {
+            _serviceManager = serviceManager;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> PrintReport()
+        {
+            var renderFormat = "PDF";
+            var extension = "pdf";
+            var mimetype = "application/pdf";
+
+            using var report = new LocalReport();
+
+            var customerRegistrationDto = await _serviceManager.CustomerService.GetCustomerRegistrationsByYear(2024);
+
+            report.DataSources.Add( new ReportDataSource("dsCustomerRegistartion", customerRegistrationDto));
+
+            var parameters = new[]
+            {
+                new ReportParameter("param1", "Customer Registration RDLC Report")
+            };
+
+            report.ReportPath = $"{_webHostEnvironment.WebRootPath}\\Reports\\rptCustomerRegistration.rdlc";
+
+            report.SetParameters(parameters);
+
+            var pdf = report.Render(renderFormat);
+
+            return File(pdf, mimetype, "report." + extension);
+
+        }
+        
+    }
+}
